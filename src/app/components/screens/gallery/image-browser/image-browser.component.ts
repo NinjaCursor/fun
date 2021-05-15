4de77763b76c9dd3c4c4327443fd5b38272019e2
Jacob Brink <jacobpeterbrink@gmail.com>
2021-05-15 13:53:44 -0400

    all thumbnails are constant size, only number of thumbnails change

--- src/app/components/screens/gallery/image-browser/image-browser.component.ts
+++ src/app/components/screens/gallery/image-browser/image-browser.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-image-browser',
@@ -11,8 +11,8 @@ export class ImageBrowserComponent implements OnInit {
     targetThumbnailWidth: number = 200;
     targetThumbnailHeight: number = 150;
 
-    thumbnailWidth: number = 120;
-    thumbnailHeight: number = 100;
+    thumbnailWidth: number = this.targetThumbnailWidth;
+    thumbnailHeight: number = this.targetThumbnailHeight;
 
     @ViewChild('browserElement') browserElement: ElementRef;
     safe: boolean = false;
@@ -35,15 +35,12 @@ export class ImageBrowserComponent implements OnInit {
     }
 
     resizeGallery(): void {
-	let hack = 1;
+	let hack = 1; // it seems that without this, the dimensions of the native element end up being 1 pixel too big. I assume rounding happens.
 	let numRows = Math.floor((this.browserElement.nativeElement.offsetHeight - hack) / this.targetThumbnailHeight);
 	let numColumns = Math.floor((this.browserElement.nativeElement.offsetWidth - hack) / this.targetThumbnailWidth);
 	this.images = new Array(numRows * numColumns);
-	this.thumbnailWidth = this.browserElement.nativeElement.offsetWidth / numColumns - hack;
-	this.thumbnailHeight = this.browserElement.nativeElement.offsetHeight / numRows - hack;
-	// console.log(`rows: ${numRows}, columns: ${numColumns} ${numRows*numColumns}`);
-	// console.log(`row pixels: ${this.browserElement.nativeElement.offsetWidth}, column pixels: ${this.browserElement.nativeElement.offsetHeight}`);
-	// console.log(`Target Width: ${this.thumbnailWidth} Target Height: ${this.thumbnailHeight}`);
+	//this.thumbnailWidth = this.browserElement.nativeElement.offsetWidth / numColumns - hack;
+	//this.thumbnailHeight = this.browserElement.nativeElement.offsetHeight / numRows - hack;
     }
 
 }

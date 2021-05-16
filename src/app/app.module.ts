0aa2d7117aa7da9b62283a280f7e5cd3c11aa367
Jacob Brink <jacobpeterbrink@gmail.com>
2021-05-15 21:25:52 -0400

    Broken :(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageBrowserComponent } from './components/screens/gallery/image-browser/image-browser.component';
 import { ImageThumbnailComponent } from './components/screens/gallery/image-thumbnail/image-thumbnail.component';
+import { GalleryScreenComponent } from './components/screens/gallery/gallery-screen/gallery-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImageBrowserComponent,
-    ImageThumbnailComponent
+    ImageThumbnailComponent,
+    GalleryScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+      AppRoutingModule,
+      MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
